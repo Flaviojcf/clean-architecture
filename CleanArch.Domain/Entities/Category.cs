9151df7aea64384170c475d4415eb52e62cc597f
Flavio <fjcf@poli.br>
2023-08-06 18:24:27 -0300

    feat: create new class

---
+++ CleanArch.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArch.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CleanArch.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArch.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+    }
+}

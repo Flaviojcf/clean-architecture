5ee47bb69098acf547cea968e0da317100278c86
Flavio <fjcf@poli.br>
2023-08-06 18:42:03 -0300

    feat: set validation

--- CleanArch.Domain/Entities/Category.cs
+++ CleanArch.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArch.Domain.Validation;
+using System;
 using System.Collections.Generic;
 
 namespace CleanArch.Domain.Entities
@@ -10,15 +11,23 @@ namespace CleanArch.Domain.Entities
 
         public Category(string name)
         {
-            Name = name;
+            ValidateDomain(name);
         }
 
         public Category(int id, string name)
         {
+            DomainExceptionValidation.When(id < 0, "Invalid id value");
             Id = id;
-            Name = name;
+            ValidateDomain(name);
         }
 
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+            Name = name;
+        }
     }
 }

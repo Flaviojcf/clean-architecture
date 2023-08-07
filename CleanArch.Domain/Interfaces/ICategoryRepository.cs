1c365a33410dd3152aa85c0d2336e0a38acc8635
Flavio <fjcf@poli.br>
2023-08-06 21:02:10 -0300

    feat: create product and category interfaces

---
+++ CleanArch.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetById(int id);
+        Task<Category> CreateCategory(Category category);
+        Task<Category> UpdateCategory(Category category);
+        Task<Category> DeleteCategory(Category category);
+
+    }
+}
---
+++ CleanArch.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetById (int id);
+        Task<Product> GetProductByCategory(int id);
+        Task<Product> CreateCategory(Product product);
+        Task<Product> UpdateCategory(Product product);
+        Task<Product> DeleteCategory(Product product);
+    }
+}

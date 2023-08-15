293e22038ea94859c397fb9ab9e6307acb53c56e
Flavio <fjcf@poli.br>
2023-08-15 19:07:05 -0300

    feat: set entities from fluent api

--- CleanArch.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArch.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using CleanArch.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CleanArch.Infra.Data.Context
 {

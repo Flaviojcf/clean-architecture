584894ffa217e43f4f7237d56386d671e7b3b5d6
Flavio <fjcf@poli.br>
2023-08-15 20:09:57 -0300

    feat: add CategoryRepository implementations

--- CleanArch.Infra.Data/Repositories/CategoryRepository.cs
+++ CleanArch.Infra.Data/Repositories/CategoryRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CleanArch.Domain.Entities;
 using CleanArch.Domain.Interfaces;
 using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -17,27 +18,33 @@ namespace CleanArch.Infra.Data.Repositories
 
         public async Task<Category> CreateCategory(Category category)
         {
-            throw new System.NotImplementedException();
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
         }
 
         public async Task<Category> DeleteCategory(Category category)
         {
-            throw new System.NotImplementedException();
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
         }
 
         public async Task<Category> GetById(int id)
         {
-            throw new System.NotImplementedException();
+            return await _categoryContext.Categories.FindAsync(id);
         }
 
         public async Task<IEnumerable<Category>> GetCategories()
         {
-            throw new System.NotImplementedException();
+            return await _categoryContext.Categories.ToListAsync();
         }
 
         public async Task<Category> UpdateCategory(Category category)
         {
-            throw new System.NotImplementedException();
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
         }
     }
 

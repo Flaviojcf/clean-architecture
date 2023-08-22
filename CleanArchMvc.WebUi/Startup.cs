6c0f17f9efd0388284e35e0b1488b6e9b221991b
Flavio <fjcf@poli.br>
2023-08-22 20:54:00 -0300

    feat: create categoryController

--- CleanArchMvc.WebUi/Startup.cs
+++ CleanArchMvc.WebUi/Startup.cs
@@ -52,7 +52,7 @@ namespace CleanArchMvc.WebUi
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Categories}/{action=Index}/{id?}");
             });
         }
     }

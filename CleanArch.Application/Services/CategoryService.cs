cb145f9404fbdc8b793f421284933186773067b7
Flavio <fjcf@poli.br>
2023-08-22 20:13:33 -0300

    feat: create ICategoryService and CategoryService

---
+++ CleanArch.Application/Services/CategoryService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper) {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Add(CategoryDTO categoryDTO)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.CreateCategory(categoryEntity);
+        }
+
+        public async Task Delete(int id)
+        {
+            var categoryEntity = _categoryRepository.GetById(id).Result;
+
+            await _categoryRepository.DeleteCategory(categoryEntity);
+        }
+
+        public async Task<CategoryDTO> GetById(int id)
+        {
+            var categoryEntity = await _categoryRepository.GetById(id);
+            return _mapper.Map<CategoryDTO>(categoryEntity);
+        }
+
+        public async Task <IEnumerable<CategoryDTO>> GetCategories()
+        {
+            var categoriesEntity = await _categoryRepository.GetCategories();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
+        }
+
+        public async Task Update(CategoryDTO categoryDTO)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryDTO);
+            await _categoryRepository.UpdateCategory(categoryEntity);
+        }
+    }
+}

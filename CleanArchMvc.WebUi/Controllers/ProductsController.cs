c1b171f0e78c3c79e18eff4efbe80181961da1d1
Flavio <fjcf@poli.br>
2023-08-23 18:49:13 -0300

    feat: add product create view

--- CleanArchMvc.WebUi/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUi/Controllers/ProductsController.cs
@@ -1,5 +1,9 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -7,9 +11,11 @@ namespace CleanArchMvc.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -18,5 +24,24 @@ namespace CleanArchMvc.WebUI.Controllers
             var products = await _productService.GetProducts();
             return View(products);
         }
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+   
     }
 }

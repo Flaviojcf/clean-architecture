9806fcf3b01f72dc1cd502237a5bbb9624f3d574
Flavio <fjcf@poli.br>
2023-08-23 18:29:28 -0300

    feat: create edit category view

--- CleanArchMvc.WebUi/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUi/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using CleanArch.Application.DTOs;
 using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -32,6 +33,37 @@ namespace CleanArchMvc.WebUI.Controllers
             if(ModelState.IsValid) 
             {
                 await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null) return NotFound();
+
+            var categoryVM = await _categoryService.GetById((int)id);
+
+            if(categoryVM == null) return NotFound();
+
+            return View(categoryVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(category);
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+          
                 return RedirectToAction(nameof(Index));
             }
             return View(category);

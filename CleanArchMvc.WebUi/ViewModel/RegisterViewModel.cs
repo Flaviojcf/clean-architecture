f73b729cc3066e0368ee4291fb07925cdc03ba65
Flavio <fjcf@poli.br>
2023-08-25 22:55:11 -0300

    feat: create login and register view model

---
+++ CleanArchMvc.WebUi/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CleanArchMvc.WebUI.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Passwords don´t match")]
+        public string ConfirmPassword { get; set; }
+    }
+}

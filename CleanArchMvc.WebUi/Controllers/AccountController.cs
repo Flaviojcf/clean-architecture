443f0eb2d93ddc163e8d0e1d5c9806b3892f9522
Flavio <fjcf@poli.br>
2023-08-25 22:56:34 -0300

    feat: create account controller

---
+++ CleanArchMvc.WebUi/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authentication;
+        public AccountController(IAuthenticate authentication)
+        {
+            _authentication = authentication;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

e5b7cdd1f0d0ff04d33bb26d1fdfb7ac01d2cab8
Flavio <fjcf@poli.br>
2023-08-25 23:43:30 -0300

    feat: add injection to seedUserRole

--- CleanArchMvc.WebUi/Startup.cs
+++ CleanArchMvc.WebUi/Startup.cs
@@ -1,3 +1,4 @@
+using CleanArch.Domain.Account;
 using CleanArch.Infra.IOC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,7 +30,8 @@ namespace CleanArchMvc.WebUi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+                    ISeedUserRoleInitial seedUserRoleInitial)
         {
             if (env.IsDevelopment())
             {
@@ -45,6 +47,12 @@ namespace CleanArchMvc.WebUi
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            seedUserRoleInitial.SeedRoles();
+
+            seedUserRoleInitial.SeedUsers();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 

bff54887d3cde5630e54a5b769aa5883537871ec
Flavio <fjcf@poli.br>
2023-08-25 23:00:15 -0300

    feat: create actions to login and register

--- CleanArchMvc.WebUi/Controllers/AccountController.cs
+++ CleanArchMvc.WebUi/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArch.Domain.Account;
+using CleanArchMvc.WebUI.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
@@ -11,9 +13,61 @@ namespace CleanArchMvc.WebUI.Controllers
             _authentication = authentication;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authentication.Authenticate(model.Email, model.Password);
+
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.(password must be strong).");
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var result = await _authentication.RegisterUser(model.Email, model.Password);
+
+            if (result)
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid register attempt (password must be strong.");
+                return View(model);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _authentication.Logout();
+            return Redirect("/Account/Login");
         }
     }
 }

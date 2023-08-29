d76225ce92bc6bb7840c1aea0a17f148c5f37b3a
Flavio <fjcf@poli.br>
2023-08-29 19:17:56 -0300

    feat: add get categories to api

---
+++ CleanArch.API/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if(categories == null)
+            {
+                return NotFound("Categories nof found");
+            }
+            return Ok(categories);
+        }
+    }
+}

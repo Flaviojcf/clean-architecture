fa6a952818b065c18142099c7fc8ee808b29b301
Flavio <fjcf@poli.br>
2023-08-31 14:10:49 -0300

    feat: create login,register and user models

---
+++ CleanArch.API/Models/RegisterModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CleanArch.API.Models
+{
+    public class RegisterModel
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Passwords don´t match")]
+        public string ConfirmPassword { get; set; }
+    }
+}

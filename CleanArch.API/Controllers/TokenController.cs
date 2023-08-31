8864eaa6dde701b707e1ffb5a99bcdbbca296a7e
Flavio <fjcf@poli.br>
2023-08-31 16:54:47 -0300

    refactor: change SecretKey to minimum characters in HmacSha156

---
+++ CleanArch.API/Controllers/TokenController.cs
@@ -0,0 +1,101 @@
+﻿using CleanArch.API.Models;
+using CleanArch.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authentication;
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IAuthenticate authentication, IConfiguration configuration)
+        {
+            _authentication = authentication ??
+                throw new ArgumentNullException(nameof(authentication));
+            _configuration = configuration;
+        }
+
+        [HttpPost("CreateUser")]
+        public async Task<ActionResult> CreateUser([FromBody] LoginModel loginModel)
+        {
+            var result = await _authentication.RegisterUser(loginModel.Email, loginModel.Password);
+
+            if (result)
+            {
+                return Ok($"User {loginModel.Email} was created successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel loginModel)
+        {
+            var result = await _authentication.Authenticate(loginModel.Email, loginModel.Password);
+
+            if (result)
+            {
+                return GenerateToken(loginModel);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
+        private UserToken GenerateToken(LoginModel loginModel)
+        {
+            //declarações do usuário
+            var claims = new[]
+            {
+                new Claim("email", loginModel.Email),
+                new Claim("meuvalor", "oque voce quiser"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //gerar chave privada para assinar o token
+            var privateKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            //gerar a assinatura digital
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            //definir o tempo de expiração
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //gerar o token
+            JwtSecurityToken token = new JwtSecurityToken(
+                //emissor
+                issuer: _configuration["Jwt:Issuer"],
+                //audiencia
+                audience: _configuration["Jwt:Audience"],
+                //claims
+                claims: claims,
+                //data de expiracao
+                expires: expiration,
+                //assinatura digital
+                signingCredentials: credentials
+                );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+    }
+}
